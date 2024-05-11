3dcb44c130143c7b1cb79330f3cb3b91e0561416
Sanskar Pandey <sanskarpandey281@gmail.com>
2024-05-11 23:52:50 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <main data-theme="light" className="h-screen">
+      <section className="bg-gradient-to-r from-teal-400 via-green-500 to-emerald-600 section-container">
+        <div className="wave wave1"></div>
+        <div className="wave wave2"></div>
+        <div className="wave wave3"></div>
+      </section>
+    </main>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+import daisyui from 'daisyui';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
+  plugins: [daisyui],
+}
+
